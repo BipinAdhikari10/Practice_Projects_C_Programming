f8547fbbcbe54318edd54f6217e17352a0bc1b00
Bipin Adhikari <adhikaribipin1032@gmail.com>
2023-02-20 16:17:32 +0545

    C program to find area of rectangle using pointer

---
+++ C program to find area of rectangle using pointer/program.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main() {
+    int length, width;
+    int *pLength = &length;
+    int *pWidth = &width;
+    int area;
+
+    printf("Enter the length of the rectangle: ");
+    scanf("%d", pLength);
+
+    printf("Enter the width of the rectangle: ");
+    scanf("%d", pWidth);
+
+    area = (*pLength) * (*pWidth);
+    printf("The area of the rectangle is %d", area);
+
+    return 0;
+}
